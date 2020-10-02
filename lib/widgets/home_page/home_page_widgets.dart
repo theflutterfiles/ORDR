a0c00d19dfe3aeb78a82e5a7a92059c9bdd82dcb
tinikapasinetti <tinikapasinetti@hotmail.com>
2020-10-02 22:24:54 +1000

    Update home_page_widgets.dart

--- lib/widgets/home_page/home_page_widgets.dart
+++ lib/widgets/home_page/home_page_widgets.dart
@@ -2,16 +2,17 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app_mindful_lifting/models/Project.dart';
 import 'package:flutter_app_mindful_lifting/screens/add_project_views/add_project_modal.dart';
-import 'package:flutter_app_mindful_lifting/screens/individual_project_views/ProjectView.dart';
 import 'package:flutter_app_mindful_lifting/services/auth.dart';
 import 'package:flutter_app_mindful_lifting/styles/colour_styles.dart';
 import 'package:flutter_app_mindful_lifting/styles/text_styles.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 final AuthService _auth = new AuthService();
 
-Stream<QuerySnapshot> getProjectsSnapshot(BuildContext context) async* { //* allows data to contsantly be open, always open stream
+Stream<QuerySnapshot> getProjectsSnapshot(BuildContext context) async* {
+  //* allows data to contsantly be open, always open stream
 
   final Firestore _firestore = Firestore.instance;
   final currentUserUID = await _auth.getCurrentUserUID();
@@ -32,15 +33,16 @@ String getInitial({String string, int limitTo}) {
 }
 
 void _showBottomSheet(context) {
-
-  Project project = new Project(null, null, null, null, null, null, null, null, null, null, null, null, null);
+  Project project = new Project(null, null, null, null, null, null, null, null,
+      null, null, null, null, null);
 
   showBottomSheet(
       context: (context),
       builder: (BuildContext context) {
-        return AddProjectModal(project: project,);
-      }
-      );
+        return AddProjectModal(
+          project: project,
+        );
+      });
 }
 
 class MenuIcon extends StatelessWidget {
@@ -52,6 +54,7 @@ class MenuIcon extends StatelessWidget {
         color: Color(0xFF333333),
         size: 25.0,
       ),
+      onPressed: () {},
     );
   }
 }
@@ -65,6 +68,7 @@ class SearchIcon extends StatelessWidget {
         color: Color(0xFF333333),
         size: 25.0,
       ),
+      onPressed: () {},
     );
   }
 }
@@ -108,7 +112,6 @@ class HeadingText extends StatelessWidget {
 class ProjectsList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     final screenHeight = MediaQuery.of(context).size.height;
     final screenWidth = MediaQuery.of(context).size.width;
 
@@ -117,82 +120,74 @@ class ProjectsList extends StatelessWidget {
         Align(
           alignment: Alignment.center,
           child: Container(
-              padding: EdgeInsets.all(20),
-              height: 0.55 * screenHeight,
-              width: 0.9 * screenWidth,
-              decoration: AppThemeColours.BlueGreenGradientBox,
-              child: StreamBuilder(
-                  stream: getProjectsSnapshot(context),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.waiting) {
-                      return Center(
-                        child: SpinKitRing(
-                          color: Colors.teal,
-                          size: 50.0,
-                        ),
-                      );
-                    } else {
-                      return Container(
+            padding: EdgeInsets.all(20),
+            height: 0.55 * screenHeight,
+            width: 0.9 * screenWidth,
+            decoration: AppThemeColours.BlueGreenGradientBox,
+            child: StreamBuilder(
+                stream: getProjectsSnapshot(context),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: SpinKitRing(
+                        color: Colors.teal,
+                        size: 50.0,
+                      ),
+                    );
+                  } else {
+                    return Container(
                         child: ListView.builder(
-                          scrollDirection: Axis.vertical,
-                          shrinkWrap: true,
-                          itemCount: snapshot.data.documents.length,
-                          itemBuilder: (context, index) {
-                            List<String> projects = new List<String>();
-                            projects.add(snapshot.data.documents[index]
-                                .data["projectName"],);
-                            return InkWell(
-                              onTap: () {
-                                //Navigator.push(context, PageRouteBuilder)
-                              },
-                              child: Card(
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(20.0),
-                                ),
-                                color: Color(0xFFEBEBEB),
-                                child: ListTile(
-                                  onTap: () async {
-                                    Navigator.push(context,
-                                        new MaterialPageRoute(builder: (context) => ProjectView(projectName: projects[index],)));
-                                  },
-                                  leading: Container(
-                                    padding: EdgeInsets.only(right: 12.0),
-                                    decoration: new BoxDecoration(
-                                        border: new Border(
-                                            right: new BorderSide(
-                                                width: 1.0,
-                                                color: Color(0xFF333333)))),
-                                    child: CircleAvatar(
-                                      backgroundColor: Color(0xFF333333),
-                                      foregroundColor: Colors.white,
-                                      child: Text(
-                                        getInitial(
-                                            string: snapshot.data.documents[index]
-                                                .data["projectName"],
-                                            limitTo: 1),
-                                      ),
-                                    ),
-                                  ),
-                                  title: Text(
-                                    snapshot.data.documents[index].data["projectName"],
-                                    style: GoogleFonts.workSans(
-                                        textStyle: AppThemes.listText),
-                                  ),
-                                  // subtitle: Text("Intermediate", style: TextStyle(color: Colors.white)),
-                                  trailing: Icon(Icons.keyboard_arrow_right,
-                                      color: Color(0xFF333333), size: 30.0),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                      );
-                    }
-                  })),
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            itemCount: snapshot.data.documents.length,
+                            itemBuilder: (BuildContext context, int index) =>
+                                _buildProjectCard(
+                                    context, snapshot.data.documents[index])));
+                  }
+                }),
+          ),
         ),
       ],
     );
   }
+}
+
+Widget _buildProjectCard(BuildContext context, DocumentSnapshot project) {
+  return Card(
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(20.0),
+    ),
+    color: Color(0xFFEBEBEB),
+    child: InkWell(
+      child: ListTile(
+        onTap: () async => {},
+        leading: Container(
+          padding: EdgeInsets.only(right: 12.0),
+          decoration: new BoxDecoration(
+              border: new Border(
+                  right: new BorderSide(width: 1.0, color: Color(0xFF333333)))),
+          child: CircleAvatar(
+            backgroundColor: Color(0xFF333333),
+            foregroundColor: Colors.white,
+            child: Text(""
+                //getInitial(string: project['projectName'], limitTo: 1),
+                ),
+          ),
+        ),
+        title: Text(
+          project['projectName'],
+          style:
+              GoogleFonts.workSans(textStyle: AppThemes.listText, fontSize: 20),
+        ),
+        subtitle: Text(
+          "Last Edited: ${DateFormat('dd MMM yyyy').format(project['lastEdited'].toDate()).toString()}",
+          style: TextStyle(fontSize: 13),
+        ),
+        trailing: Icon(Icons.keyboard_arrow_right,
+            color: Color(0xFF333333), size: 30.0),
+      ),
+    ),
+  );
 }
 
 class LogoImage extends StatelessWidget {
