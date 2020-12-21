1a5f89ba6f0ca809a9ebc98fe1db597069fd46a5
theflutterfiles <75551212+theflutterfiles@users.noreply.github.com>
2020-12-21 20:52:20 +1100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,78 +1,40 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_app_mindful_lifting/notifiers/auth_notifier.dart';
-import 'package:flutter_app_mindful_lifting/notifiers/checklist_notifier.dart';
-import 'package:flutter_app_mindful_lifting/notifiers/menu_drawer_notifier.dart';
-import 'package:flutter_app_mindful_lifting/notifiers/task_notifier.dart';
-import 'package:flutter_app_mindful_lifting/screens/authenticate/login.dart';
-import 'package:flutter_app_mindful_lifting/screens/collaborators_view/CollaboratorsView.dart';
-import 'package:flutter_app_mindful_lifting/screens/dashboard_view/ProjectView.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter_app_mindful_lifting/screens/events_view/EventsView.dart';
-import 'package:flutter_app_mindful_lifting/screens/tasks_view/TasksView.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_app_mindful_lifting/notifiers/menu_notifier.dart';
+import 'package:flutter_app_mindful_lifting/screens/individual_project_views/ProjectView.dart';
+import 'package:flutter_app_mindful_lifting/screens/individual_project_views/TasksView.dart';
 import 'package:provider/provider.dart';
+import 'screens/individual_project_views/homeScreen.dart';
 
-import 'notifiers/project_notifier.dart';
-import 'screens/home_view/homeScreen.dart';
-
-void main() => runApp(MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (context) => ProjectNotifier(),
-        ),
-        ChangeNotifierProvider(
-          create: (context) => AuthNotifier(),
-        ),
-        ChangeNotifierProvider<MenuDrawerNorifier>(
-          create: (_) => MenuDrawerNorifier(),
-        ),
-        ChangeNotifierProvider<TaskNotifier>(create: (_) => TaskNotifier()),
-        ChangeNotifierProvider<ChecklistNotifier>(
-            create: (_) => ChecklistNotifier()),
+void main() => runApp(
+      MultiProvider(providers: [
+        ChangeNotifierProvider(create: (context) => MenuDrawerNotifier())
       ],
       child: MyApp(),
-    ));
+      ),
+    );
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
-    //wrap wrapper in provider
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        appBarTheme: AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          appBarTheme: AppBarTheme(
+            color: Colors.white,
+            elevation: 0,
+          ),
+          scaffoldBackgroundColor: Colors.white,
+          canvasColor: Colors.transparent,
+          focusColor: Color(0xFF333333),
         ),
-        scaffoldBackgroundColor: Colors.white,
-        canvasColor: Colors.transparent,
-        focusColor: Color(0xFF333333),
-      ),
-      //the widgets inside here can access the values provided by stream provider value above e.g. user data from Stream<User> get user
-      home: Consumer<AuthNotifier>(
-        builder: (context, notifier, child) {
-          return notifier.user != null ? HomePage() : Login();
-        },
-      ),
-      localizationsDelegates: [
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      routes: {
-        "home": (_) => HomePage(),
-        "tasks": (_) => TasksView(),
-        "login": (_) => Login(),
-        "dashboard": (_) => ProjectDetailView(),
-        "collaborators": (_) => CollaboratorsView(),
-        "events": (_) => EventsView(),
-      },
-      supportedLocales: [
-        const Locale('en', ''),
-        //const Locale('aus', ''),
-      ],
-    );
+        home: HomePage(),
+        routes: {
+          "home": (_) => HomePage(),
+          "tasks": (_) => TasksView(),
+          "projects": (_) => ProjectsView(),
+        });
   }
 }
